111b7c8892e098219177de7dc1e8b04f852558d3
Chitandaaaaa <707320242@qq.com>
2020-06-15 01:06:21 +0800

    init

---
+++ src/main/java/com/hammer/blog/web/IndexController.java
@@ -0,0 +1,11 @@
+package com.hammer.blog.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+}
